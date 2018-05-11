77605aafc80b68552e1b2fe54e264200837cf90f
doksel <doksel7@gmail.com>
2018-05-11 13:40:16 +0300

    last commit

--- js/showForm.js
+++ js/showForm.js
@@ -1,20 +1,20 @@
 function showForm() {
-    const form = document.querySelector('.form');
+    const form = doc.querySelector('.form');
     form.hidden = false;
-    const iconPlus = document.querySelector('.icon');
+    const iconPlus = doc.querySelector('.icon');
     iconPlus.style.transform='rotate(45deg)';
 }
 function hiddenForm() {
-    const form = document.querySelector('.form');
+    const form = doc.querySelector('.form');
     form.hidden = true;
-    const iconPlus = document.querySelector('.icon');
+    const iconPlus = doc.querySelector('.icon');
     iconPlus.style.transform='rotate(90deg)';
 }
-function showForm_editMovies() {
-    const form = document.querySelector('.form_editMovies');
+function showForm_editMovie() {
+    const form = doc.querySelector('.form_editMovies');
     form.hidden = false;
 }
-function hiddenForm_editMovies() {
-    const form = document.querySelector('.form_editMovies');
+function hiddenForm_editMovie() {
+    const form = doc.querySelector('.form_editMovies');
     form.hidden = true;
 }
